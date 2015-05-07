8d3550e89ab3edc7429ecf58c0f571495a2b537d
gonthalo <gonzalogalpmg@gmail.com>
2015-05-07 23:04:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-function nada(){}
-function boton(var numero){
-	function nuevoton(){
-
-	}
-	for (var bla=1; bla<=numero; bla++){
-		nuevoton();
-	}
-}
-function aprieta_boton(){}
+function nada(){};
+//function boton(numero){
+//	function nuevoton(){
+//
+//	}
+//	for (var bla=1; bla<=numero; bla++){
+//		nuevoton();
+//	}
+//}
+//function aprieta_boton(){}
 function edad(){
-	alert ("Tengo 17 años");
+	var fecha = Date();
+	var lista = [31,28,31,30,31,30,31,31,30,31,30,31];
+	var dia = fecha.getDate() - 24;
+	var mes = fecha.getMonth() - 12;
+	var anio = fecha.getFullYear() - 1997;
+	if (dia<0){
+		dia = dia + lista[mes + 11];
+		mes = mes - 1;
+	}
+	if (mes<0){
+		mes = mes + 12;
+		anio = anio - 1;
+	}
+	alert ("Mi edad es de " + anio + " años");
 }
