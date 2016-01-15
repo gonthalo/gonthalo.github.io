3904b9b22df1e5e4b5443d6468480a4f5fbdb58e
Gonthalo <gonzalogalpmg@gmail.com>
2016-01-15 23:26:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-function nada(){}
+var tabla = document.getElementById("tabla");
+var filas = [];
+var matriz = [];
 
-function nuevoton(){}
-
-function boton(numero){
-	for (var bla=1; bla<=numero; bla++){
-		nuevoton();
+function iniciar(){
+	for(var ii=0; ii<5; ii++){
+		tabla.innerHTML = tabla.innerHTML + '<tr id="fila' + ii +'"></tr>';
+		matriz[ii] = [];
+		aux = document.getElementById("fila" + ii);
+		for (var jj=0; jj<5; jj++){
+			aux.innerHTML = aux.innerHTML + '<td id="fil' + ii +'col' + jj + '"></td>';
+			matriz[ii][jj] = document.getElementById("fil" + ii +"col" + jj);
+			matriz[ii][jj].innerHTML = "Hola!";
+		}
 	}
 }
-function aprieta_boton(){}
+
+function boton(strin, x0, y0){
+	matriz[x0][y0].innerHTML = '<input type="button" value="" onclick="' + strin + '">';
+}
+
+function nada(){}
+
+iniciar();
+
+boton('window.location="http://gonthalo,github.io"', 2, 3);
+
+window.setInterval(nada(), 200);
