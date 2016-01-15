753334f6b3735b9d7b48f65490958ffbbd0a21ff
Gonthalo <gonzalogalpmg@gmail.com>
2016-01-16 00:37:35 +0100

    Update reloj.js

--- reloj.js
+++ reloj.js
@@ -1,30 +1,50 @@
 var tabla = document.getElementById("tabla");
 var filas = [];
 var matriz = [];
+var enlaces = ["http://gonthalo.github.io", "http://xkcd.com", "http://gonthalo.github.io/eSdlB.html"];
+var colorines = ["rgb(5, 250, 100)", "rgb(5, 20, 200)", "white"]
+var num_filas = 15;
+var num_col = 15;
 
 function iniciar(){
-	for(var ii=0; ii<5; ii++){
+	for(var ii=0; ii<num_filas; ii++){
 		tabla.innerHTML = tabla.innerHTML + '<tr id="fila' + ii +'"></tr>';
 		matriz[ii] = [];
 		aux = document.getElementById("fila" + ii);
-		for (var jj=0; jj<5; jj++){
+		for (var jj=0; jj<num_col; jj++){
 			aux.innerHTML = aux.innerHTML + '<td id="fil' + ii +'col' + jj + '"></td>';
-			matriz[ii][jj] = document.getElementById("fil" + ii +"col" + jj);
-			matriz[ii][jj].innerHTML = "Hola!";
+			document.getElementById("fil" + ii +"col" + jj).innerHTML = '<input type="button" value="    " onclick="nada()" id="f' + ii +'c' + jj + '">';
+			//document.getElementById("f" + ii +"c" + jj).background = "white";
+			matriz[ii][jj] = "white";
 		}
 	}
 }
 
-function boton(strin, x0, y0){
-	document.getElementById("fil" + x0 + "col" + y0).innerHTML = '<input type="button" value="" onclick="' + strin + '">';
+function enlace(numero){
+	window.location=enlaces[numero];
+}
+
+function boton(num, x0, y0){
+	console.log([num, x0, y0]);
+	document.getElementById("fil" + x0 + "col" + y0).innerHTML = '<input type="button" value="    " onclick="enlace(' + num + ')">';
+	matriz[x0][y0] = colorines[num];
+	document.getElementById("fil" + x0 + "col" + y0).style.backgroundColor = colorines[num];
+}
+
+function actualizar(){
+	r = parseInt(Math.random()*num_col*num_filas);
+	c = matriz[r%num_filas][parseInt(r/num_filas)];
+	for(var tt=0; tt<num_filas*num_col; tt++){
+		if(matriz[tt%num_filas][parseInt(tt/num_filas)]==c && tt!=r){
+			k = parseInt(Math.random()*enlaces.length);
+			boton(k, r%num_filas, parseInt(r/num_filas));
+			return
+		}
+	}
 }
 
 function nada(){}
 
 iniciar();
-
-function enlace(){
-	window.location="http://gonthalo.github.io";
-}
-
-boton("enlace()", 2, 3);
+boton(0, 2, 3);
+window.setInterval(actualizar, 200);
