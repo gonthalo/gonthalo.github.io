a24dcc0edd32ccaa2490de68c4e157ca86e3ff9f
Gonthalo <gonzalogalpmg@gmail.com>
2016-01-15 17:09:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-function nada(){};
-//function boton(numero){
-//	function nuevoton(){
-//
-//	}
-//	for (var bla=1; bla<=numero; bla++){
-//		nuevoton();
-//	}
-//}
-//function aprieta_boton(){}
-function edad(){
-	var fecha = new Date();
-	var d = new Date("December 24, 1997 11:13:00");
-	fecha = fecha - d; 
-	var lista = [31,28,31,30,31,30,31,31,30,31,30,31];
-	alert ("Mi edad es de " + anio + " años");
+function nada(){}
+
+function nuevoton(){}
+
+function boton(numero){
+	for (var bla=1; bla<=numero; bla++){
+		nuevoton();
+	}
 }
+function aprieta_boton(){}
